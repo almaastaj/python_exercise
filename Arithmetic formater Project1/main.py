fe3f90a04909db38d262fd6e5a7c06a95ce46829
Almaas Taj <almaastaj123@gmail.com>
2023-04-22 20:46:18 +0530

    Checked all testcases

--- Arithmetic formater Project1/main.py
+++ Arithmetic formater Project1/main.py
@@ -4,7 +4,7 @@ from pytest import main
 from arithmetic_arranger import arithmetic_arranger
 
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"],True))
 
 
 # Run unit tests automatically
